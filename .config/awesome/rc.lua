40552f4a301861086b94f4058b855db654d3f6d0
Beau Grantham <beau.grantham@va.gov>
2020-07-05 13:36:45 -0400

    [awesomewm] Scroll tasklist to min/restore

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -166,11 +166,15 @@ local tasklist_buttons = gears.table.join(
   awful.button({        }, 3, function()
     awful.menu.client_list({ theme = { width = 250 } })
   end),
-  awful.button({        }, 4, function ()
-    awful.client.focus.byidx(1)
+  awful.button({        }, 4, function (c)
+    c.minimized = true
   end),
-  awful.button({        }, 5, function ()
-    awful.client.focus.byidx(-1)
+  awful.button({        }, 5, function (c)
+    c:emit_signal(
+      "request::activate",
+      "tasklist",
+      { raise = true }
+    )
   end)
 )
 
