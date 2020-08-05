79ba17b876187068e0948f613880253c28f2effc
Beau Grantham <beau.grantham@va.gov>
2020-08-05 09:44:04 -0400

    [awesomewm] Add custom theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -65,10 +65,10 @@ modkey = "Mod4"
 awful.layout.layouts = {
   awful.layout.suit.tile.left,
   awful.layout.suit.tile,
-  awful.layout.suit.spiral,
-  awful.layout.suit.fair,
-  awful.layout.suit.max,
   awful.layout.suit.floating,
+  -- awful.layout.suit.spiral,
+  -- awful.layout.suit.fair,
+  -- awful.layout.suit.max,
   -- awful.layout.suit.tile.bottom,
   -- awful.layout.suit.tile.top,
   -- awful.layout.suit.fair.horizontal,
